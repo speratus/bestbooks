7a05f4aa8d2502807ab2c5a1980c4fe313ec43cc
Andrew Luchuk <andrew.luchuk@outlook.com>
2022-03-24 23:19:39 -0400

    adds a view for viewing a book detail

--- booklist/views.py
+++ booklist/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Book
+
+
+def book_detail(request, book_slug):
+    book = get_object_or_404(Book, slug=book_slug)
+    authors = book.author.all()
+    review = book.reviews.all()
+
+    return render(request, 'book.html', {'book': book, 'authors': authors, 'review': review})
+
